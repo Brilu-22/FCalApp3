7a8d1eced6bd01c0b8d1cfda6a291a7b797aad7a
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-11-18 16:52:20 +0200

    startup

--- FitCalBackend/MyFitnessBackend/obj/Debug/net9.0/MyFitnessBackend.AssemblyInfo.cs
+++ FitCalBackend/MyFitnessBackend/obj/Debug/net9.0/MyFitnessBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe42bcd14a5d32431100a6062e20132f84387e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2798a6c3f2266e4a9af31ff28c0bfd329c2e9519")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
